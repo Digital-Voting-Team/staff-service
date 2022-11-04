f410f411f95570075a6eea0b2b5e721598fd0aba
Kiril R <kyryl.ryabov@gmail.com>
2022-11-04 16:23:27 +0200

    Added vendor. Actions

---
+++ vendor/github.com/Digital-Voting-Team/auth-serivce/endpoints/validateToken.go
@@ -0,0 +1,32 @@
+package endpoints
+
+import (
+	"encoding/json"
+	"github.com/Digital-Voting-Team/auth-serivce/resources"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"net/http"
+)
+
+func ParseJwtResponse(r *http.Response) (*resources.JwtResponse, error) {
+	var response resources.JwtResponse
+
+	if err := json.NewDecoder(r.Body).Decode(&response); err != nil {
+		return &response, errors.Wrap(err, "failed to unmarshal")
+	}
+
+	return &response, nil
+}
+
+func ValidateToken(token, endpoint string) (*resources.JwtResponse, error) {
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", token)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseJwtResponse(res)
+}
